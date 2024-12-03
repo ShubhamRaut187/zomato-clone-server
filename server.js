6cc55c2264198891d6b5ee8743fa7f7b309c298f
Shubham Raut <shubhamraut@KNRX-IN-C-ENG-SHUBHAMRAUT.local>
2024-12-03 12:58:10 +0530

    Added database configurations & connected database

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+// Database Connection
+const { databaseConnection } = require('./DatabaseConfigurations/config')
+
 const app = express();
 
 app.use(express.json());
@@ -13,8 +16,16 @@ app.get('/',(req,res)=>{
     res.send('Connected to backend server of Zomato Clone');
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log('========================================================================');
-    console.log('Server connected successfully on port: ', process.env.PORT);
-    console.log('========================================================================');
+app.listen(process.env.PORT, async ()=>{
+    try {
+        await databaseConnection;
+        console.log('========================================================================');
+        console.log('Server connected successfully to database on port: ', process.env.PORT);
+        console.log('========================================================================');
+    } catch (error) {
+        console.log('========================================================================');
+        console.log('Failed to connect server & database on port: ', process.env.PORT);
+        console.log('Connection error: ', error);
+        console.log('========================================================================');
+    }
 })
