e9225e7311c8bda787a32aa64958f9917674d1c5
ShubhamRaut187 <raut.shubham4554@gmail.com>
2024-12-07 10:08:40 +0530

    Implemented cookie auth. Modified folder structure

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 // Database Connection
 const { databaseConnection } = require('./DatabaseConfigurations/config')
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.send('Connected to backend server of Zomato Clone');
