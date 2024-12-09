0ab51bf71d4a8ec8373cb10590affc20b6696888
Shubham Raut <shubhamraut@KNRX-IN-C-ENG-SHUBHAMRAUT.local>
2024-12-09 11:57:18 +0530

    Added routes for fetching user

--- Routes/User/UserRouter/index.js
+++ Routes/User/UserRouter/index.js
@@ -7,7 +7,7 @@ userRouter.post('/api/v1/signup', async (req, res)=>{
    
     if (!req.body || Object.keys(req.body).length === 0) {
         res.status(204).json({
-            error: true,
+            error: false,
             message: 'Request has no body.'
         });
         return;
@@ -45,3 +45,68 @@ userRouter.post('/api/v1/signup', async (req, res)=>{
    }
 
 });
+
+userRouter.get('/api/v1/users',async(req,res) => {
+    // Todo: Add filtering options in API using query params.
+    try {
+        
+        let users = await userModel.find({});
+        users = users.map((user) => {
+            return delete user.password;
+        })
+        res.status(200).json({
+            error: false,
+            message: 'Users fetched sucessfully',
+            users,
+        });
+
+    } catch (error) {
+        
+        res.status(501).json({
+            error: true,
+            message: 'Internal server error.',
+            cause: error,
+        });
+
+    }
+});
+
+userRouter.get('api/v1/users/:userId', async(req,res) => {
+    try {
+
+        const userId = req.params.userId;
+        // Validation to check for userId in the path params in request.
+        if (!userId || typeof userId !== 'string') {
+            res.status(204).json({
+                error: false,
+                message:'Request does notinclude userId.',
+            });
+            return;
+        }
+
+        const user = await userModel.findOne({ _id: userId });
+        // Vaidation to check if the user is found in DB.
+        if (!user || Object.keys(req.body).length === 0) {
+            res.status(404).json({
+                error: false,
+                message: 'No user found.',
+            });
+            return;
+        }
+
+        res.status(200).json({
+            error: false,
+            message: 'Users fetched sucessfully.',
+            user,
+        })
+
+    } catch (error) {
+        
+        res.status(501).json({
+            error: true,
+            message: 'Internal server error.',
+            cause: error,
+        });
+
+    }
+})
