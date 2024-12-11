c53c37b61b83ba0826272822cd2bb7c729fabd3d
Shubham Raut <shubhamraut@KNRX-IN-C-ENG-SHUBHAMRAUT.local>
2024-12-11 14:13:36 +0530

    Added a middleware to check authorization is only for platform admin

--- Middlewares/Authorization/index.js
+++ Middlewares/Authorization/index.js
@@ -44,6 +44,45 @@ const isUserAuthorized = async(req,res) => {
    }    
 };
 
+
+const isPlatformAdmin = async(req, res) => {
+    const adminId = req.userId;
+    if (!adminId || typeof adminId !== 'string') {
+        
+        console.log('Authorization Middleware for admin: ', 'adminId not found.')
+        res.status(401).json({
+            error: false,
+            message: 'Admin is not authenticated. Authentication failed while checking Authorization.'
+        });
+
+        return;
+    }
+
+    try {
+        const admin = await adminModel.findOne({ _id: adminId });
+
+        if ((admin && admin.category === 'super-admin-system') || (admin && admin.category === 'general-admin-system')) {
+            next();
+        }
+        
+        res.status(401).json({
+            error: false,
+            message: 'Admin not authorized to block user.'
+        });
+
+    } catch (error) {
+
+        console.log('Error at authorization middleware: ', error);
+        res.status(501).json({
+            error: true,
+            message: 'Internal server error at authorization.',
+            cause: error,
+        });
+
+    }
+}
+
 module.exports = {
     isUserAuthorized,
+    isPlatformAdmin,
 }
