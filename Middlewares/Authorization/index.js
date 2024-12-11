65e14200558e2a31746bea5c751ca9192c801c61
Shubham Raut <shubhamraut@KNRX-IN-C-ENG-SHUBHAMRAUT.local>
2024-12-11 12:16:06 +0530

    Added middleware to check authorization for the user request

---
+++ Middlewares/Authorization/index.js
@@ -0,0 +1,49 @@
+const { adminModel }  = require('../../../DataSchema/Admin');
+
+// Middleware to check the user request authorization.
+const isUserAuthorized = async(req,res) => {
+    const userId = req.userId;
+    if (!userId || typeof userId !== 'string') {
+        
+        console.log('Authorization Middleware: ', 'userId not found.')
+        res.status(401).json({
+            error: false,
+            message: 'User is not authenticated. Authentication failed while checking Authorization.'
+        });
+
+        return;
+
+    }
+
+   try {
+
+        const userIdFromParams = req.body.userId;
+        const admin = await adminModel.findOne({ _id: userId });
+
+        if (userId === userIdFromParams) {
+            next();
+        }
+
+        if ((admin && admin.category === 'super-admin-system') || (admin && admin.category === 'general-admin-system')) {
+            next();
+        }
+
+        res.status(401).json({
+            error: false,
+            message: 'User is not authorized to perform the request.'
+        });
+
+   } catch (error) {
+    
+        console.log('Error at authorization middleware: ', error);
+        res.status(501).json({
+            error: true,
+            message: 'Internal server error at authorization.',
+            cause: error,
+        });
+   }    
+};
+
+module.exports = {
+    isUserAuthorized,
+}
